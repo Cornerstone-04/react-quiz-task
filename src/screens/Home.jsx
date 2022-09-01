d9b32ac5f6dd7fca06253d9dc8fa82355ff3b20c
Cornerstone Ephraim <fortunecornerstone@gmail.com>
2022-09-01 01:25:44 +0100

    added favicon & changed app name

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "../styles/home.css";
 import toast, { Toaster } from "react-hot-toast";
 
